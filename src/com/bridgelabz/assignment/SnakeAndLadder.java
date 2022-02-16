b37df4c67ee53d0ade5ae24e6b84bb9ce6a2c473
Subhashree naik <subhashree.naik@gmail.com>
2022-02-16 19:02:41 +0530

    welcome message

---
+++ src/com/bridgelabz/assignment/SnakeAndLadder.java
@@ -0,0 +1,18 @@
+
+package com.bridgelabz.assignment;
+
+/**
+ * @author subhashree
+ *
+ */
+public class SnakeAndLadder {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Snake And Ladder");
+
+	}
+
+}
