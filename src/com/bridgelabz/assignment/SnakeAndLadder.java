2d5d320ee885dd3326b9e131773ba58a9bba6893
Subhashree naik <subhashree.naik@gmail.com>
2022-02-16 19:20:05 +0530

    Added:Dice rolled program

--- src/com/bridgelabz/assignment/SnakeAndLadder.java
+++ src/com/bridgelabz/assignment/SnakeAndLadder.java
@@ -6,12 +6,38 @@ package com.bridgelabz.assignment;
  *
  */
 public class SnakeAndLadder {
+	
+	//Declaring variables
+	static int start_Position=0;
+	static int diceRolled=0;
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int diceValue = (int)(Math.random() *6)+1;
+		switch(diceValue) {
+		case 1:
+			System.out.println("Dice rolled and the value is 1");
+			break;
+		case 2:
+			System.out.println("Dice rolled and the value is 2");
+			break;
+		case 3:
+			System.out.println("Dice rolled and the value is 3");
+			break;
+		case 4:
+			System.out.println("Dice rolled and the value is 4");
+			break;
+		case 5:
+			System.out.println("Dice rolled and the value is 5");
+			break;
+		default:
+			System.out.println("Dice rolled and the value is 6");
+			break;
+			
 		
-		System.out.println("Welcome to Snake And Ladder");
+		}
+		
 
 	}
 
