8e14edb3588ae4c718db647b88403e2fea437ccc
Subhashree naik <subhashree.naik@gmail.com>
2022-02-16 23:40:58 +0530

    Snake ladder by two player

--- src/com/bridgelabz/assignment/SnakeAndLadder.java
+++ src/com/bridgelabz/assignment/SnakeAndLadder.java
@@ -7,48 +7,36 @@ package com.bridgelabz.assignment;
  */
 public class SnakeAndLadder {
 	
-	//Declaring variables
-	static int position=0;
-	static int diceRolled=0;
-	
-	
-	//Method to create random dice value
-	static int diceRolled() {
+	static int rollDice() {
 		int diceValue = (int)(Math.random() *6)+1;
-		switch(diceValue) {
-		case 1:
-			return 1;	
-		case 2:
-			return 2;
-		case 3:
-			return 3;
-		case 4:
-			return 4;
-		case 5:
-			return 5;
-		default:
-			return 6;
-		}	
+		return diceValue;
 	}
 	
-	static void checkOption(int diceValue) {
-		int options = (int)(Math.random() *3)+1;
-		
+	static int pickOption() {
 		//option 1 is no play
 		//option 2 is ladder
 		//option 3 is snake
-		if(options==1) {
+		int options = (int)(Math.random() *3)+1;
+		return options;
+	}
 	
-		}
-		else if(options==2) {
+	static int play(int position) {
+		
+		
+		int diceValue=rollDice();
+		int optionType= pickOption();
+		if (optionType==1) {
 			
+		}
+		else if(optionType==2) {
 			position=position+diceValue;
 			
 			if(position >100) {
 				position = position-diceValue;
 			}
+			
+			position=play(position);
 		}
-		
 		else {
 			position=position-diceValue;
 			
@@ -56,28 +44,35 @@ public class SnakeAndLadder {
 				position=0;
 			}
 		}
-		
-		System.out.println("Position is  "+position+ "");
-		
-		
-		
+		return position;
 	}
 
-
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		while(position<100) {
-	  		int dicevValue=diceRolled();
-	  		diceRolled++;
-	  		checkOption(dicevValue);
-	  		}
-		System.out.println("Number of times the dice is rolled:"+ diceRolled);
-	  		
+		int position_player1=0;
+		int position_player2=0;
+		
+		while(true) {
+			
+			position_player1=play(position_player1);
+			System.out.println("position of player1 is"+position_player1);
+			if(position_player1==100) {
+				System.out.println("player1 wins");
+				break;
+			}
+			
+			position_player2=play(position_player2);
+			System.out.println("position of player2 is"+ position_player2);
+			if(position_player2==100) {
+				System.out.println("player2 wins");
+				break;
+			}
+			
+		}
 		
 		
+
 	}
 
 }
